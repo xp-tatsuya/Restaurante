a38987a33c54f0ef67298b63ca72bdb8b1e73db5
sistemanoturno <sistemanoturno@LAB5-06.sistemafieto.rede>
2025-04-04 21:28:36 -0300

    Criação da Tela login e home, validação de Login.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,56 @@
 package application;
 	
+import java.io.IOException;
 import java.sql.Connection;
 
 import ConnectionFactory.ConnectionDatabase;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
+	
+	private static Stage stage;
+	private static Scene Login;
+	private static Scene main;
+	
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			stage = primaryStage;
+			primaryStage.setTitle("Miyazaki - Login");
+			
+			Parent fxmlLogin = FXMLLoader.load(getClass().getResource("/View/ViewLogin.fxml"));
+			Login = new Scene(fxmlLogin);
+			
+			primaryStage.setScene(Login);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void changeScreen(String tela) {
+		if(tela.equals("main")) {
+			stage.setScene(main);
+			stage.centerOnScreen();
+			stage.setTitle("Menu Principal");
+		}
+	}
+	
+	public static void TelaHome () throws IOException {
+		FXMLLoader fxmlHome = new FXMLLoader();
+		fxmlHome.setLocation(Main.class.getResource("/View/ViewHome.fxml"));
+		Parent telaHome = fxmlHome.load();
+		main = new Scene(telaHome);
+		stage.setTitle("Miyazaki - Menu Principal");
+		stage.setScene(main);
+		stage.setResizable(false);
+		stage.centerOnScreen();
+		stage.show();
 	}
 	
 	public static void main(String[] args) {
@@ -28,5 +58,6 @@ public class Main extends Application {
 		ConnectionDatabase.closeConnection(con);
 		launch(args);
 	}
+	
 }
 	
--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,10 @@
 module Restaurante {
 	requires javafx.controls;
 	requires java.sql;
+	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens ConnectionFactory to javafx.fxml, javafx.base;
+	opens Controller to javafx.graphics, javafx.fxml;
 }
