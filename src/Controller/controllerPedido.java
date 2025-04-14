0b4800d09e4ec0706be36c38fdff8781251d950a
sistemanoturno <sistemanoturno@LAB5-05.sistemafieto.rede>
2025-04-14 20:51:10 -0300

    alterações na tela de relatorio pedido

---
+++ src/Controller/controllerPedido.java
@@ -0,0 +1,116 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class controllerPedido {
+
+    @FXML
+    private Button btAdicionar;
+
+    @FXML
+    private Button btCardapio;
+
+    @FXML
+    private Button btEditar;
+
+    @FXML
+    private Button btExcluir;
+
+    @FXML
+    private Button btFinalizar;
+
+    @FXML
+    private Button btFonecedor;
+
+    @FXML
+    private Button btFuncionario;
+
+    @FXML
+    private Button btHome;
+
+    @FXML
+    private Button btMesa;
+
+    @FXML
+    private Button btPedido;
+
+    @FXML
+    private Button btProduto;
+
+    @FXML
+    private Button btSair;
+
+    @FXML
+    private TableView<?> tablePedidos;
+
+    @FXML
+    private TextField txtPesquisa;
+
+    @FXML
+    private Label txtUser;
+
+    @FXML
+    void ActionAdicionar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionCardapio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionEditar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionExcluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionFinalizar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionFornecedor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionFuncionario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionMesa(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionPedido(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionProduto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionSair(ActionEvent event) {
+
+    }
+}
