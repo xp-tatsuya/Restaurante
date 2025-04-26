3653d7861e3cc8fea53a538295d6d36389b5434b
sistemanoturno <sistemanoturno@LAB5-05.sistemafieto.rede>
2025-04-25 21:46:55 -0300

    muchas telas sendo feita pelo MAGNIFICO GRANDIOSO E PODEROSO "FRON END"🍅

---
+++ Controller/controllerAddMesa.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class controllerAddMesa {
+
+    @FXML
+    private Button btCancelar;
+
+    @FXML
+    private Button btSalvar;
+
+    @FXML
+    private ChoiceBox<?> choiceCondicao;
+
+    @FXML
+    private TextField txtCapacidade;
+
+    @FXML
+    void ActionCancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionSalvar(ActionEvent event) {
+
+    }
+
+}
