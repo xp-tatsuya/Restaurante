ff29a17bac63cfc8ab76fcddb52973bdead385bc
sistemanoturno <sistemanoturno@LAB5-05.sistemafieto.rede>
2025-05-19 20:46:32 -0300

    tela rsgistro garçom 🍅

--- Controller/controllerRegistroGarcons.java
+++ Controller/controllerRegistroGarcons.java
@@ -3,27 +3,74 @@ package Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class controllerRegistroGarcons {
 
     @FXML
+    private Button btAdicionar;
+
+    @FXML
     private Button btCancelar;
 
     @FXML
-    private Button btSalvar;
+    private Button btExcluir;
 
     @FXML
-    private TextField txtCodigoMesa;
+    private Button btFinalizar;
+
+    @FXML
+    private TableColumn<?, ?> columnIndice;
+
+    @FXML
+    private TableColumn<?, ?> columnObs;
+
+    @FXML
+    private TableColumn<?, ?> columnPreconUn;
+
+    @FXML
+    private TableColumn<?, ?> columnProduto;
+
+    @FXML
+    private TableColumn<?, ?> columnQuantidade;
+
+    @FXML
+    private TableColumn<?, ?> columnTotalTabela;
+
+    @FXML
+    private Label labelDesconto;
+
+    @FXML
+    private Label labelTotal;
+
+    @FXML
+    private TableView<?> tablePedido;
+
+    @FXML
+    private TextField txtDesconto;
 
     @FXML
     private TextField txtFuncionario;
 
     @FXML
-    private TextField txtProdutoVendido;
+    private TextField txtMesa;
 
     @FXML
-    private TextField txtSenha;
+    private TextField txtObs;
+
+    @FXML
+    private TextField txtProduto;
+
+    @FXML
+    private TextField txtQuantidade;
+
+    @FXML
+    void ActionAdicionar(ActionEvent event) {
+
+    }
 
     @FXML
     void ActionCancelar(ActionEvent event) {
@@ -31,7 +78,12 @@ public class controllerRegistroGarcons {
     }
 
     @FXML
-    void ActionSalvar(ActionEvent event) {
+    void ActionExcluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionFinalizar(ActionEvent event) {
 
     }
 
